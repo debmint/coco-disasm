adbf72d742cc93bd92af065cfda0d3075639e163
debmint <debmint@users.noreply.github.com>
2006-09-05 17:11:23 +0000

    Moved amode AM_DRCT from position 7 to correct position 11. This corrects default classes for the Y, U, S indexed, and the Direct modes

--- os9disasm/amodes.h
+++ os9disasm/amodes.h
@@ -12,21 +12,21 @@
 #define AM_YIMM	 4	/*  Y Immediate		*/
 #define AM_UIMM	 5	/*  U Immediate		*/
 #define AM_SIMM	 6	/*  S Immediate		*/
-#define AM_DRCT	 7	/*  Direct	*/
-#define AM_XIDX	 8	/*  X-Indexed	*/
+#define AM_XIDX	 7	/*  X-Indexed	*/
 	/* The following 3 classes do not apear in dtble.h - and never explicitly
 	 * mentioned in the source.  They are here to reserve space in the
 	 * Y,U,and S indexed modes, and are given names for clarity
 	 */
-#define AM_YIDX	 9	/*  Y-Indexed	*/
-#define AM_UIDX	10	/*  U-Indexed	*/
-#define AM_SIDX	11	/*  S-Indexed	*/
+#define AM_YIDX	 8	/*  Y-Indexed	*/
+#define AM_UIDX	 9	/*  U-Indexed	*/
+#define AM_SIDX	10	/*  S-Indexed	*/
 
 	/* All the modes above _MUST_ remain in the exact order
 	 * for processing in the function Domode (in cmdfile.c
 	 */
 
 #define AM_BYTI	 1	/*  1-byte immediate	*/
+#define AM_DRCT 11	/*  Direct	*/
 #define AM_EXT	12	/*  Extended	*/
 #define AM_REL	13	/*  Relative	*/
 /* Classes below don't affect addressing modes */
